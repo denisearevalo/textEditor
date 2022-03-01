4d7be385592d6b545db370c17511e112397c5892
Denise Arevalo <denisearev@gmail.com>
2022-02-28 20:17:53 -0800

    completed todos

---
+++ client/src/js/database.js
@@ -0,0 +1,41 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('JATE', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('JATE')) {
+        console.log('JATE database already exists');
+        return;
+      }
+      db.createObjectStore('JATE', { keyPath: 'id', autoIncrement: true });
+      console.log('JATE database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  console.log('PUT to the database');
+  const jateDb = await openDB('JATE', 1);
+  const tx = jateDb.transaction('JATE', 'readwrite');
+  const store = tx.objectStore('JATE');
+  const request = store.put({ todo: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET all from the database');
+  const jateDb = await openDB('JATE', 1);
+  const tx = jateDb.transaction('JATE', 'readonly');
+  const store = tx.objectStore('JATE');
+  // const request = store.getAll();
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
+initdb();
